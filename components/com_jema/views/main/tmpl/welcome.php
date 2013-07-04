fe3e01258240380adc080a7e02577da3000e523e
fanghornn <fanghornn@gmail.com>
2013-07-05 01:17:38 +0200

    maj vue main

--- components/com_jema/views/main/tmpl/welcome.php
+++ components/com_jema/views/main/tmpl/welcome.php
@@ -7,11 +7,22 @@
 defined('_JEXEC') or die('Not that way');
 ?>
 
+<img class="jema_banner"src="images/banners/ban.png"/>
 
-<h1>Hello World !</h1>
+
+<br/><br/>
+
 <div class="jema_cadre_menu"> 
 	<ul><a href="/jean-massat/">Accueil</a></a></ul>
 	<ul><a>Blog</a></ul>
 	<ul><a>C.V.</a></ul>
 	<ul><a>A propos</a></ul>
+</div>
+
+<div class="jema_content">
+	<h1>Hello World !</h1>
+
+
+
+
 </div>
