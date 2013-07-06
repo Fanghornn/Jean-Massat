06a94f7b22f507154e6ebdf0f283e1ce8122ea0b
fanghornn <fanghornn@gmail.com>
2013-07-06 15:12:29 +0200

    view blog

---
+++ components/com_jema/views/blog/tmpl/blog.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * projet: JeanMassat | JEMA
+ * Author: Jean Baptiste PRIAM MASSAT
+ * 
+ */
+defined('_JEXEC') or die('Not that way');
+require_once(JPATH_COMPONENT.DS.'helpers'.DS.'Class'.DS.'PHP'.DS.'jema_header.php');
+?>
+
+
+<div class="jema_content">
+	<h2 class="jema_view_title">Blog - Jean Massat</h2>
+
+	<div>
+		Site en construction
+	</div>
+</div>
+
+<div>
+	Footer - 
+</div>
