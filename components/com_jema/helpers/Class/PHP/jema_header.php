7ead5db276893dd7afbc712c9f79296885d5b6ab
fanghornn <fanghornn@gmail.com>
2013-07-06 15:16:47 +0200

    premiere class php pour le header

---
+++ components/com_jema/helpers/Class/PHP/jema_header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * projet: JeanMassat | JEMA
+ * Author: Jean Baptiste PRIAM MASSAT
+ * 
+ */
+defined('_JEXEC') or die('Not that way');
+?>
+
+<div class="jema_cadre_menu"> 
+	<ul class="jema_menu_btn"><a class="btn btn-success jema_btn_width" href="/jean-massat/">Accueil</a></a></ul><br/>
+	<ul class="jema_menu_btn"><a class="btn btn-success jema_btn_width" href="/jean-massat/index.php/blog" >Blog</a></ul><br/>
+	<ul class="jema_menu_btn"><a class="btn btn-success jema_btn_width" href="/jean-massat/index.php/cv">C.V.</a></ul><br/>
+	<ul class="jema_menu_btn"><a class="btn btn-success jema_btn_width" href="/jean-massat/index.php/about">A propos</a></ul>
+</div>
