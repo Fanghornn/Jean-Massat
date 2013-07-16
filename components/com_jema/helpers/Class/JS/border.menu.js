2d3abc273c51bd2244d893bb191954ad799225bf
fanghornn <fanghornn@gmail.com>
2013-07-16 13:25:26 +0200

    Menu version finale (esperons)

--- components/com_jema/helpers/Class/JS/border.menu.js
+++ components/com_jema/helpers/Class/JS/border.menu.js
@@ -6,75 +6,41 @@
 var BorderMenu = new Class({
 	initialize: function(){
 		var self = this,
-		buttonSlide = new Element('i', {class:'jema_menu_btn_slide icon-home', html:'<a href:"#h2"></a>'}),
+		classicMenu = new Element('div', {class:'jema_toggle_classic_menu', html:'Menu'}),
 		cadreMenu = document.getElement('.jema_cadre_menu'),
 		btnHome = document.getElement('.jema_btn_home'),
 		btnBlog = document.getElement('.jema_btn_blog'),
 		btnCv = document.getElement('.jema_btn_cv'),
 		btnAbout = document.getElement('.jema_btn_about');
 
-		//Init positionnement
-		cadreMenu.grab(buttonSlide);
-		self.showMenu(cadreMenu);
-
-		/**
-		 * Events Cadre
-		 */
-		cadreMenu.addEvent('mouseenter', function(){
-			self.showMenu(cadreMenu);
-		});
-		
-		cadreMenu.addEvent('mouseleave', function(){
-			self.discardMenu(cadreMenu);
-		});
-
+		classicMenu.inject(btnHome, 'before');
 		/**
 		 * Event animation btn
 		 */
-		btnHome.addEvent('mouseenter', function(){
-			self.animateButton(btnHome);
-		});
-		btnBlog.addEvent('mouseenter', function(){
-			self.animateButton(btnBlog);
-		});
-		btnCv.addEvent('mouseenter', function(){
-			self.animateButton(btnCv);
-		});
-		btnAbout.addEvent('mouseenter', function(){
-			self.animateButton(btnAbout);
-		});
+		self.animateButton(btnHome);
+		self.animateButton(btnBlog);
+		self.animateButton(btnCv);
+		self.animateButton(btnAbout);
 
 		/**
 		 * Event reset animation btn
 		 */
-		btnHome.addEvent('mouseleave', function(){
-			self.inanimateButton(btnHome, 'home');
-		});
-		btnBlog.addEvent('mouseleave', function(){
-			self.inanimateButton(btnBlog, 'blog');
-		});
-		btnCv.addEvent('mouseleave', function(){
-			self.inanimateButton(btnCv, 'cv');
-		});
-		btnAbout.addEvent('mouseleave', function(){
-			self.inanimateButton(btnAbout, 'about');
-		});
-	}
-	/*Change left style property to simulate a slide effect */
-	,showMenu: function(cadreMenu){
-		cadreMenu.setStyle('left', '-.2em');
-	}
-	/*Reset Cadre position*/
-	,discardMenu: function(cadreMenu){
-		cadreMenu.setStyle('left', '-11.2em');
+		self.inanimateButton(btnHome, 'home');
+		self.inanimateButton(btnBlog, 'blog');
+		self.inanimateButton(btnCv, 'cv');
+		self.inanimateButton(btnAbout, 'about');
 	}
 	,animateButton: function(el){
-		el.setStyle('margin-left', '0px');
-		el.getElement('.icon-fast-forward').destroy();
+		el.addEvent('mouseenter', function(){
+			el.setStyle('margin-left', '0px');
+			el.getElement('.icon-fast-forward').destroy();
+		});
 	}
 	,inanimateButton: function(el, name){
-		el.setStyle('margin-left', '-9em');
-		var resuractedIcon = new Element('i', {class:'icon-fast-forward jema_icon_menu jema_icon_'+name});
-		el.grab(resuractedIcon);
-	}
+		el.addEvent('mouseleave', function(){
+			el.setStyle('margin-left', '-9em');
+			var resuractedIcon = new Element('i', {class:'icon-fast-forward jema_icon_menu jema_icon_'+name});
+			el.grab(resuractedIcon);
+		});
+	}	
 });
--- components/com_jema/helpers/Class/PHP/jema_header.php
+++ components/com_jema/helpers/Class/PHP/jema_header.php
@@ -7,39 +7,49 @@
 defined('_JEXEC') or die('Not that way');
 ?>
 <br/><br/>
+
 <div class="jema_cadre_menu"> 
-	<br/><br/><br/><br/>
 	<a class="jema_btn jema_btn_home" href="/jean-massat/">.:: Accueil ::.<i class="icon-fast-forward jema_icon_menu jem_icon_home"></a></i><br/><br/>
 	<a class="jema_btn jema_btn_blog" href="/jean-massat/index.php/blog" >.:: Blog ::.<i class="icon-fast-forward jema_icon_menu jema_icon_blog"></a></i><br/><br/>
 	<a class="jema_btn jema_btn_cv" href="/jean-massat/index.php/cv">.:: Dev ::.<i class="icon-fast-forward jema_icon_menu jema_icon_cv"></a></i><br/><br/>
 	<a class="jema_btn jema_btn_about" href="/jean-massat/index.php/about">.:: A propos ::.<i class="icon-fast-forward jema_icon_menu jema_icon_about"></a></i><br/>
 </div>
-<div class="jema_cadre_profil jema_hidden">
-	<span class="jema_cadre_profil_pic">
-		<img src="/jean-massat/images/images_jema/gentlemen.jpg" width="200"/>
-	</span>
-	<br/>
 
-	<div class="jema_cadre_profil_infos">
-		<div>
-			Jean-Baptiste Priam Massat 
-			<br/>
-			---------------------
-			<br/> 
-			Développeur d'applications
-			<br/>
-			---------------------
-			<br/>
-			<a href="//plus.google.com/107279035521866382037?prsrc=3"
-   			rel="publisher" target="_top" style="text-decoration:none;display:inline-block;color:#333;text-align:center; font:13px/16px arial,sans-serif;white-space:nowrap;">
-			<span style="display:inline-block;font-weight:bold;vertical-align:top;margin-right:5px; margin-top:8px;">Jean-Baptiste</span><span style="display:inline-block;vertical-align:top;margin-right:15px; margin-top:8px;">on</span>
-			<img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a>
-			<br/><br/>
-			Mon profil Github: <iframe src="http://ghbtns.com/github-btn.html?user=Fanghornn&type=follow" allowtransparency="true" frameborder="0" scrolling="0" width="150" height="20"></iframe>
-			<br/><br/>
-			Profil Steam(pour les gamer):<br/>
-			<a href="http://steamcommunity.com/id/kesai/" target="_blank"><img src="http://badges.steamprofile.com/profile/default/steam/76561197982614240.png" border="0" alt="Steamprofile badge by Steamprofile.com"></a>
+<div class="jema_content">
+
+	<div class="jema_cadre_profil jema_hidden">
+
+		<span class="jema_cadre_profil_pic">
+
+			<img src="/jean-massat/images/images_jema/gentlemen.jpg" width="200"/>
+
+		</span>
+
+		<br/>
+
+		<div class="jema_cadre_profil_infos">
+
+			<div>
+				Jean-Baptiste Priam Massat 
+				<br/>
+				---------------------
+				<br/> 
+				Développeur d'applications
+				<br/>
+				---------------------
+				<br/>
+				<a href="//plus.google.com/107279035521866382037?prsrc=3"
+	   			rel="publisher" target="_top" style="text-decoration:none;display:inline-block;color:#333;text-align:center; font:13px/16px arial,sans-serif;white-space:nowrap;">
+				<span style="display:inline-block;font-weight:bold;vertical-align:top;margin-right:5px; margin-top:8px;">Jean-Baptiste</span><span style="display:inline-block;vertical-align:top;margin-right:15px; margin-top:8px;">on</span>
+				<img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a>
+				<br/><br/>
+				Mon profil Github: <iframe src="http://ghbtns.com/github-btn.html?user=Fanghornn&type=follow" allowtransparency="true" frameborder="0" scrolling="0" width="150" height="20"></iframe>
+				<br/><br/>
+				Profil Steam(pour les gamer):<br/>
+				<a href="http://steamcommunity.com/id/kesai/" target="_blank"><img src="http://badges.steamprofile.com/profile/default/steam/76561197982614240.png" border="0" alt="Steamprofile badge by Steamprofile.com"></a>
+
+			</div>
+			
 		</div>
-	</div>
 
-</div>
+	</div>
