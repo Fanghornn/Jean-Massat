2e9a73f526a88f6077157ff2feb8f6270d17960f
fanghornn <fanghornn@gmail.com>
2013-07-22 19:00:00 +0200

    [PHP]Blog - Modifications de l'affichage du HTML

--- components/com_jema/views/blog/tmpl/blog.php
+++ components/com_jema/views/blog/tmpl/blog.php
@@ -13,24 +13,45 @@ require_once(JPATH_COMPONENT.DS.'helpers'.DS.'Class'.DS.'PHP'.DS.'jema_header.ph
 	<?php 
 		$dbresponse = JemaModelMain::getArticle();
 		foreach ($dbresponse as $row){
-			echo "<div class=\"jema_article_cadre\"><h3 class=\"jema_article_title\">".$row["titre_article"]."</h3><br/>";
-			echo "<img class=\"jema_article_image\" src='/jean-massat/".$row["img_src"]."'/><br/><br/>";
-			echo "<p class=\"jema_article_content\">".$row["content_article"]."</p></div><br/><br/>"; 
+	?>
+	<div class="jema_article_cadre">
+		<h3 class="jema_article_title">
+			<?php echo $row["titre_article"]; ?>
+		</h3><br/>
+		<img class="jema_article_image" src="<?php echo $row["img_src"]; ?>"/><br/><br/>
+		<p class="jema_article_content">
+			<?php echo $row["content_article"]; ?>
+		</p>
+	</div>
+	<br/><br/>
+	<?php 
 		}
 	?>
-
 	<br/>
 	<?php
 		$user = JFactory::getUser();
 
-		if(isset($user->groups[8]) || isset($user->groups[7])){
-			echo "<div class=\"jema_article_creator\">Bonjour Administrateur, Veux tu créer un nouvel article ?<br/>";
-			echo "<span class=\"btn btn-success jema_display_creator\">Création article</span></div><br/>";
-			echo "<div class=\"jema_hidden jema_form_article\"><form enctype=\"multipart/form-data\" action=\"index.php?option=com_jema&task=saveArticle\" method=\"post\">";
-			echo "<label>Nom de l'article:</label><input type=\"text\" name=\"titre_article\" required />";
-			echo "<label>Rédaction de l'article en lui même:</label><textarea rows=\"10\" style=\"width:600px; height:300px;\" name=\"content_article\" required ></textarea>"; 
-			echo "<label>Image représentant l'article (facultatif)</label><input type=\"file\" name=\"image_article\" size=\"20\"/>";
-			echo "<br/><br/><input type=\"submit\" value=\"Envoyer!\" class=\"btn btn-success\"/></form></div>";
+		if(isset($user->groups[8]) || isset($user->groups[7])){?>
+			<div class="jema_article_creator">
+				Bonjour Administrateur, Veux tu créer un nouvel article ?<br/>
+				<span class="btn btn-success jema_display_creator">
+					Création article
+				</span>
+			</div><br/>
+
+			<div class="jema_hidden jema_form_article">
+				<form enctype="multipart/form-data" action="index.php?option=com_jema&task=saveArticle" method="post">
+					<label>Nom de l'article:</label>
+						<input type="text" name="titre_article" required />
+					<label>Rédaction de l'article en lui même:</label>
+						<textarea rows="10" style="width:600px; height:300px;" name="content_article" required ></textarea>
+					<label>Image représentant l'article (facultatif)</label>
+						<input type="file" name="image_article" size="20"/>
+					<br/><br/>
+					<input type="submit" value="Envoyer!" class="btn btn-success"/>
+				</form>
+			</div>
+		<?php
 		}
-	?>
+		?>
 </div>
