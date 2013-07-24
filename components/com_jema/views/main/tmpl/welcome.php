ac8f7e2fd826498a44bf196c1ae37eaf385c2729
fanghornn <fanghornn@gmail.com>
2013-07-24 16:37:45 +0200

    Rédaction page d'acceuil

--- components/com_jema/views/main/tmpl/welcome.php
+++ components/com_jema/views/main/tmpl/welcome.php
@@ -7,7 +7,33 @@
 defined('_JEXEC') or die('Not that way');
 require_once(JPATH_COMPONENT.DS.'helpers'.DS.'Class'.DS.'PHP'.DS.'jema_header.php');
 ?>
+<h2 class="jema_view_title">
+	Acceuil
+</h2>
 
-	<h2 class="jema_view_title">Accueil</h2>
+<br/>
+
+<h1> 
+	Bienvenue sur le site de Fanghornn !
+</h1>
+
+<br/><br/><br/>
+
+<img class="jema_home_img" src="/jean-massat/images/images_jema/triforce.png"/>
+
+<p>
+	J'ai développé ce site dans un premier temps afin de perfectionner mes connaissances de développement 
+	acquises durant ma formation de développeur logiciel.<br/><br/>
+
+	Outre le fait de perfectionner mes compétences, ce site est pour moi l'occasion de disposer d'un blog, ce dernier, ayant principalement pour but
+	de partager ma passion du développement, internet et de l'informatique en général.<br/><br/>
+
+	Dans la partie Développement du site, se trouvera mon C.V. et mes différents travaux de développement, nottament sur les informations et les
+	détails techniques du développement de ce site.<br/><br/>
+
+	Si vous souhaitez en savoir plus sur moi, c'est dans la section "A propos" qu'il faudra vous rendre.
+
+	Je vous souhaite une bonne navigation sur Fanghornn.com !
+</p>
 
 </div>
