07a46be75ba74a508dddcc27251ceeb1b38858e0
fanghornn <fanghornn@gmail.com>
2013-07-24 16:37:15 +0200

    maj view a propos

--- components/com_jema/views/about/tmpl/about.php
+++ components/com_jema/views/about/tmpl/about.php
@@ -12,7 +12,15 @@ require_once(JPATH_COMPONENT.DS.'helpers'.DS.'Class'.DS.'PHP'.DS.'jema_header.ph
 
 	<h2 class="jema_view_title">- A propos -</h2>
 
-	<div>
-		
-	</div>
+	<p>
+		Mais c'est quoi ce Fraghom, farghon, machin truc bidule ?<br/>
+		Fanghornn tout simplement (ou presque),<br/>
+
+		C'est le pseudonyme que j'ai choisi pour mes activités sur internet (jeux ou forum) n'étant pas adepte de l'utilisation de ma réele identité sur la toile<br/>
+		Les fans du livre ou des films Le seigneur des anneaux auront surement reconnu la références à la forêt de Fangörn située dans la région du Rohan des terres du milieu<br/>
+		Au risque de passer pour un geek ridicule, je dois avouer que mon inspiration provient de cette dernière, ayant adopter ce pseudonyme à une époque (maintenant révolue)
+		ou je jouais à World Of Warcraft, j'ai du adapter l'orthographe du mot pour l'adapter a la politique du respect des copyrights pour le choix d'un nom de joueur 
+		imposés dans le jeu. <br/>
+	</p>
+
 </div>
