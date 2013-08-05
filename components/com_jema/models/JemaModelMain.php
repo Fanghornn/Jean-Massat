4abb77c90d342473958a1152604e87969560e53a
fanghornn <fanghornn@gmail.com>
2013-08-05 23:42:14 +0200

    [PHP] Modif requete SQL pour avoir la liste des article décroissants selon leur ID

--- components/com_jema/models/JemaModelMain.php
+++ components/com_jema/models/JemaModelMain.php
@@ -15,11 +15,12 @@ Class JemaModelMain extends JModelLegacy{
 		
 	}
 	
-	public static function getArticle(){
+	public static function getArticles($firstEntry, $nb_rows){
 		$db = JFactory::getDBO();
 		$query = "	SELECT a.`id_article` , `titre_article` , `content_article` , `img_src` , `date_creation`
 					FROM `#__jema_articles` AS a
-					LEFT JOIN `#__jema_articles_images` AS i ON i.id_article = a.id_article";
+					LEFT JOIN `#__jema_articles_images` AS i ON i.id_article = a.id_article
+					ORDER BY a.id_article DESC LIMIT ".$firstEntry.",".$nb_rows.";";
 		$db->setQuery($query);
 		$rows = $db->loadAssocList();
 		return $rows;
