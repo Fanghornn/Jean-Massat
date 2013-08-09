c7896acc78ca833e38a004e8c685234e42cf6df1
fanghornn <fanghornn@gmail.com>
2013-08-09 20:35:56 +0200

    Redaction partie 1 CV Non corrigée bien sûr

--- components/com_jema/views/cv/tmpl/cv.php
+++ components/com_jema/views/cv/tmpl/cv.php
@@ -9,6 +9,26 @@ require_once(JPATH_COMPONENT.DS.'helpers'.DS.'Class'.DS.'PHP'.DS.'jema_header.ph
 ?>
 	<h1 class="jema_cv_title">Projets</h2><br/>
 
+	<h2 class="jema_cv_subtitle"><a href="https://github.com/jean-massat/Jean-Massat" target="about:blank">Jean Massat (2013)</a></h3>
+	<p class="jema_cv_p">
+		Jean-Massat est le site sur lequel vous vous trouvez actuellement. Il m'a fallu un mois entier environ avant de le mettre au point avec la quasi totalité
+		des composants et fonctionnalités prévues initialements.<br/>
+
+		Pour la structure PHP de base, le projet est sous la forme d'un composant MVC Joomla. Donc un framework PHP avec une API associée (Joomla 3.1).<br/>
+
+		Les API CSS de BootStrap et Gantry qui sont des framework CSS open-source, m'ont permis de mettre en place le design général du site.<br/>
+
+		Si vous consultez ce site depuis un mobile ou une tablette, vous pouvez constatez que j'ai fais en sorte de changer des éléments d'affichage
+		afin d'obtenir un affichage propre et épuré sur les petites résolutions. Etant plutôt jeune, je sais que la navigation sur smartphone se fais de plus en plus,
+		de plus, les gens sont souvent agacés lorsqu'ils arrivent sur un site qui n'a rien prévu pour leur appreil. C'est pourquoi j'ai soigné l'adaptation aux terminaux mobiles<br/>
+
+		Après avoir appris à maitriser le framework JavaScript Mootools et sa structure orientée objets lors de mon stage de formation, je l'ai naturellement mis en oeuvre
+		lors de la concéption du JavaScript de Jean-Massat. Le menu de navigation du site (aussi bien desktop que mobile), est une class Mootools que j'ai entiérement développé.<br/>
+
+		Outre l'utilisation de fonction de Framework libres, JS ou PHP, j'ai écris manuellement l'intégralité du code de Jean-Massat. Le code source est libre et disponible sur
+		Github, un simple click de votre part sur le titre de cet article vous amennera sur sa page :). 
+	</p>
+
 	<h2 class="jema_cv_subtitle"><a href="https://github.com/art-manager/art-manager?source=cc" target="about:blank">Art-Manager (2013)</a></h3>
 	<p class="jema_cv_p">
 		C'est dans le cadre d'un stage de 10 semaines au cours de ma formation de développeur logiciels que j'ai pu participer au projet Art-Manager.
@@ -16,14 +36,6 @@ require_once(JPATH_COMPONENT.DS.'helpers'.DS.'Class'.DS.'PHP'.DS.'jema_header.ph
 		bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla 
 		bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla 
 		bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla bllabla 
-	</p>
-
-	<h2 class="jema_cv_subtitle"><a href="https://github.com/jean-massat/Jean-Massat" target="about:blank">Jean Massat (2013)</a></h3>
-	<p class="jema_cv_p">
-		Ici je vais parler du site, des techniques de dev etc.. un peu de blabla pour faire contenu après le reste
-		Ici je vais parler du site, des techniques de dev etc.. un peu de blabla pour faire contenu après le reste
-		Ici je vais parler du site, des techniques de dev etc.. un peu de blabla pour faire contenu après le reste
-		Ici je vais parler du site, des techniques de dev etc.. un peu de blabla pour faire contenu après le reste
 	</p>
 
 	<h1 class="jema_cv_title">Curriculum Vitae</h2>
